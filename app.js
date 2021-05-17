1262816c422889bc99d439a800b724d8aa229730
ZRCrawford15 <48399752+ZRCrawford15@users.noreply.github.com>
2021-05-17 14:15:13 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+let express = require('express');
+
+let app = express();
+let handlebars = require('express-handlebars').create({defaultLayout}:'main');
+let session = require('express-session');
+let bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extend: false}));
+app.use(session({secret: 'password'}));
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 23950);
+
+
+app.get('/count', function(req, res) {
+	let context = {};
+	context.count = req.session.count || 0;
+	req.session.count = context.count += 1;
+	res.render('counter', context)
+});
+
+
+app.listen(app.get('port'), function(){
+	console.log("express started on http://localhost:" + app.get('port') + "; press Ctrl-C to terminate.");
+})
